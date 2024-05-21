c977fb445dd5c1934c26925f9775d92f3711768a
danakh9797 <dana.alkhayat@masah.tech>
2024-05-21 10:55:05 +0300

    Update page.js window

--- locationDashboard/app/page.js
+++ locationDashboard/app/page.js
@@ -2,21 +2,39 @@
 
 import { useEffect, useState } from "react";
 import axios from "axios";
+import dynamic from "next/dynamic";
 import "leaflet/dist/leaflet.css";
-import L from "leaflet";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+// Dynamically import react-leaflet components
+const MapContainer = dynamic(
+  () => import("react-leaflet").then((mod) => mod.MapContainer),
+  { ssr: false }
+);
+const TileLayer = dynamic(
+  () => import("react-leaflet").then((mod) => mod.TileLayer),
+  { ssr: false }
+);
+const Marker = dynamic(
+  () => import("react-leaflet").then((mod) => mod.Marker),
+  { ssr: false }
+);
+const Popup = dynamic(() => import("react-leaflet").then((mod) => mod.Popup), {
+  ssr: false,
+});
 
 const HomePage = () => {
   const [locations, setLocations] = useState({});
+  const [icon, setIcon] = useState(null);
 
   useEffect(() => {
     console.log("start");
 
     const fetchLocations = async () => {
       try {
-        await axios
-          .get("https://testing-na3j.onrender.com/locations")
-          .then((response) => setLocations(response.data));
+        const response = await axios.get(
+          "https://testing-na3j.onrender.com/locations"
+        );
+        setLocations(response.data);
       } catch (error) {
         console.error("Error fetching locations:", error);
       }
@@ -29,41 +47,45 @@ const HomePage = () => {
     return () => clearInterval(interval);
   }, []);
 
-  const icon = L.icon({
-    iconUrl: "/images/marker-icon.png",
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-  });
+  useEffect(() => {
+    // Ensure Leaflet-specific code only runs on the client side
+    if (typeof window !== "undefined") {
+      const L = require("leaflet");
+      const icon = L.icon({
+        iconUrl: "/images/marker-icon.png",
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+      });
+      setIcon(icon);
+    }
+  }, []);
+
+  if (!icon) return null; // Ensure icon is loaded before rendering the map
 
   return (
     <div style={{ height: "100vh", width: "100%" }}>
-      {typeof window !== "undefined" && (
-        <MapContainer
-          center={[21.42664, 39.82563]}
-          zoom={13}
-          style={{ height: "100%", width: "100%" }}
-        >
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-          {Object.keys(locations).map((userId) => (
-            <Marker
-              key={userId}
-              position={[
-                locations[userId].latitude,
-                locations[userId].longitude,
-              ]}
-              icon={icon}
-            >
-              <Popup>
-                User: {userId} <br /> Last updated:{" "}
-                {new Date(locations[userId].timestamp).toLocaleTimeString()}
-              </Popup>
-            </Marker>
-          ))}
-        </MapContainer>
-      )}
+      <MapContainer
+        center={[21.42664, 39.82563]}
+        zoom={13}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {Object.keys(locations).map((userId) => (
+          <Marker
+            key={userId}
+            position={[locations[userId].latitude, locations[userId].longitude]}
+            icon={icon}
+          >
+            <Popup>
+              User: {userId} <br /> Last updated:{" "}
+              {new Date(locations[userId].timestamp).toLocaleTimeString()}
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 };
